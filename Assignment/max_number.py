98eda7ff2496d96d2efae8503841917974e1a153
Avipsha <avipshashrestha75@gnail.com>
2025-01-05 12:24:30 +0545

    Assignment

--- Assignment/max_number.py
+++
@@ -1,6 +0,0 @@
-num1 = int(input("Enter the first number: "))
-num2 = int(input("Enter the second number: "))
-num3 = int(input("Enter the third number: "))
-
-max_num = max(num1, num2, num3)
-print(f"The maximum number is {max_num}")
